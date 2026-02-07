855384b801c903e525b3ab996a0180fd6628760d
Abdallahyass <abdallahaahmad2000@gmail.com>
2026-02-07 20:08:40 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class DetailsStoreApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Details Store1', // اسم التبويب
+      title: 'Details Store11', // اسم التبويب
       theme: ThemeData(
         // 1. سر التصميم: الخلفية البيضاء النقية
         scaffoldBackgroundColor: const Color(0xFFFFFFFF),
